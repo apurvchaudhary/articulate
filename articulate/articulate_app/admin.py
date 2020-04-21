e004bb6531c15ef8a781a5d0a651d2fec73f349d
apurvchaudhary <apurv.sirohi@gmail.com>
2020-04-21 12:46:02 +0530

    admin modified

--- articulate/articulate_app/admin.py
+++ articulate/articulate_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 from articulate_app.models import Article
 
 # Register your models here.
 admin.site.register([Article])
+admin.site.unregister([Group, User])
+admin.site.site_header = 'Admin by geeky@purv ©2020'
+admin.site.site_url = "/ani/home"
