b5ec5461cdcfa337601fa17eb7871b2395f9b4ae
apurvchaudhary <apurv.sirohi@gmail.com>
2020-04-22 19:52:25 +0530

    modified models

--- articulate/articulate_app/models.py
+++ articulate/articulate_app/models.py
@@ -42,21 +42,21 @@ class Article(ModelBase):
     """
     Model to save article added by django default user
     """
-    TYPE_CHOICE = {
-        ("Series", "SERIES"),
-        ("Documentary", "DOCUMENTARY"),
-        ("Short Film", "SHORT FILM"),
-        ("Youtube", "YOUTUBE"),
-        ("Movie", "MOVIE"),
-        ("Album", "ALBUM"),
-    }
+    TYPE_CHOICE = [
+        ("Series", "series"),
+        ("Documentary", "documentary"),
+        ("Short Film", "short Film"),
+        ("Youtube", "youtube"),
+        ("Movie", "movie"),
+        ("Album", "album"),
+    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.FloatField(verbose_name="My Rating", validators=[validate_rating])
     title = models.CharField(verbose_name="Title", max_length=255)
-    type = models.CharField(verbose_name="Type", max_length=50, choices=TYPE_CHOICE, default=0)
+    type = models.CharField(verbose_name="Type", max_length=50, choices=TYPE_CHOICE, default=TYPE_CHOICE[4][0])
     creatives = models.CharField(verbose_name="Director/Creation/Youtuber", max_length=100, default="Not Mentioned")
     review = models.TextField(verbose_name="Review Text")
-    embed_link = models.URLField(verbose_name="Trailer Embed Link", blank=True, null=True)
+    embed_link = models.URLField(verbose_name="Trailer Youtube Link", blank=True, null=True)
     image_url = models.FileField(verbose_name="Movie Poster", upload_to='uploaded_images/', validators=[validate_image],
                                  help_text='Maximum file size allowed is 80kb', unique=True)
     imdb_rating =models.FloatField(verbose_name="IMDB Rating", validators=[validate_imdb_rating])
@@ -65,11 +65,16 @@ class Article(ModelBase):
 
     def save(self, *args, **kwargs):
         if self.embed_link:
-            youtube_normal_link = self.embed_link.split("&")[0]
-            hash = youtube_normal_link.split("=")[1]
-            youtube_embed_url = "https://www.youtube.com/embed/" + hash
-            self.embed_link = youtube_embed_url
-            super(Article, self).save(*args, **kwargs)
+            try:
+                youtube_normal_link = self.embed_link.split("&")[0]
+                hash = youtube_normal_link.split("=")[1]
+                youtube_embed_url = "https://www.youtube.com/embed/" + hash
+                self.embed_link = youtube_embed_url
+            except IndexError:
+                pass
+            finally:
+                super(Article, self).save(*args, **kwargs)
+        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title}"
