b7d7dcf41e1c5902de38f1ea8557e70b4dbfbe88
apurvchaudhary <apurv.sirohi@gmail.com>
2020-04-27 19:36:28 +0530

    youtube url creation modified

--- articulate/articulate_app/service.py
+++ articulate/articulate_app/service.py
@@ -1,9 +1,12 @@
 def create_embed_link(link):
+    YOUTUBE_URL = "https://www.youtube.com/embed/"
     try:
         youtube_normal_link = link.split("&")[0]
         hash = youtube_normal_link.split("=")[1]
-        youtube_embed_url = "https://www.youtube.com/embed/" + hash
-        embed_link = youtube_embed_url
-        return embed_link
+        return YOUTUBE_URL + hash
     except IndexError:
-        return link
+        try:
+            hash = link.split("/")[-1]
+            return YOUTUBE_URL + hash
+        except IndexError:
+            return link
