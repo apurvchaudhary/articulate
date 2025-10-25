e80321d66fdd9b464f6b44e213548e0442532f62
Apurv Chaudhary <apurv.chaudhary_ext@zooplus.com>
2025-10-25 11:22:55 +0530

    updated in 2025

--- articulate/articulate/urls.py
+++ articulate/articulate/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('aniadmin/', admin.site.urls),
-    path('ani/', include('articulate_app.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('articulate_app.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- articulate/articulate_app/urls.py
+++ articulate/articulate_app/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from articulate_app.views import about_page_view, HomePageView, MoviePageView, SortPageView
 
 urlpatterns = [
     re_path(r'^about/$', about_page_view, name="about"),
-    re_path(r'^home/$', HomePageView.as_view(), name="home-page"),
+    path('', HomePageView.as_view(), name="home-page"),
     re_path(r'^movie/$', MoviePageView.as_view(), name="movie-page"),
     re_path(r'^sort/$', SortPageView.as_view(), name="sort-rating"),
 ]
